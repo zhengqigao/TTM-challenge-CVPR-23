3c25d834d723a71d6d2fc65e151e6cac8f6a2896
Zhengqi Gao <zhengqi@mit.edu>
2023-06-22 08:51:32 -0500

    revise script

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # TTM-challenge-CVPR-23
 
-This is the code base we used for [Ego4d TTM challenged](https://eval.ai/web/challenges/challenge-page/1625/overview) held at CVPR'23. We secured the second place in the contest.
+This is the code base we used for [Ego4d TTM challenge](https://eval.ai/web/challenges/challenge-page/1625/overview) held at CVPR'23. We secured the second place in the contest.
 
 ## Approach
 
-A technical report could be found in this repo. Our approach is Transformer-based. We ensemble two trained model for better accuracy. The two trained models are provided in the google drive, and the script used to do the ensemble is provided in this repo.
+You can locate a technical report within this repository. Our approach relies on Transformer-based models. To enhance the performance, we have employed an ensemble of two trained models. We may explore further improvements to our approach in the future. As a result, at this moment, we can only provide two JSON files, which contain predictions from the two trained models on the test set, along with the ensemble script.
+
+## Reference
+
+We suggest preprocessing the data following:
+
+https://github.com/facebookresearch/Ego4d/blob/main/ego4d/cli/README.md
+
+https://github.com/EGO4D/social-interactions/tree/ttm/scripts
+
 
--- ensemble.py
+++ ensemble.py
@@ -1,8 +1,4 @@
 import json
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 import argparse
 
@@ -10,8 +6,8 @@ if __name__ == '__main__':
 
     argparser = argparse.ArgumentParser()
 
-    argparser.add_argument('--ensemble', type=str, default='random')
-    argparser.add_argument('--output', type=str)
+    argparser.add_argument('--ensemble', type=str, default='weighted')
+    argparser.add_argument('--output', type=str, default='./final_submit.json')
     args = argparser.parse_args()
     np.random.seed(42)
 
@@ -21,10 +17,10 @@ if __name__ == '__main__':
     prob = 0.25  # w/ prob=0.4 use file2
     weight = 0.25  # weight for file2
 
-    file1 = '../submit_ttm1.json'
-    file2 = '../submit_ttm2.json'
+    file1 = './submit_ttm1.json'
+    file2 = './submit_ttm2.json'
 
-    target_file =  args.output # '../submit_ttm5.json'
+    target_file = args.output
     # Open the JSON file in read mode
     with open(file1, 'r') as f:
         # Load the JSON data into a Python dictionary
@@ -46,7 +42,7 @@ if __name__ == '__main__':
                 elif ensemble_type == 'weighted':
                     data['results'][i]['score'] = (1 - weight) * data['results'][i]['score'] + weight * data2['results'][i]['score']
                 else:
-                    raise ValueError("Ensemble method not implemented")
+                    raise NotImplementedError("Ensemble method not implemented")
 
     # Write the dictionary to the JSON file
     with open(target_file, "w") as f:
